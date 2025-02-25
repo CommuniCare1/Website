c3090799f39f9be5f09d4890ed3dd16c835b0688
TailaiYing <taiying9627@gmail.com>
2025-02-25 13:11:44 -0500

    i think it finally works pleease ive been doing this for too long

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/Website/" : "/",
   build: {
     outDir: "dist",
   },
