21efa2dfc330ec9b745da50e19d1759508c7bc9c
TailaiYing <taiying9627@gmail.com>
2025-02-25 09:39:42 -0500

    trying to figure out this workflow stuff

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,7 +35,7 @@ function Home() {
                 <h1 className="scroll-animate mb-4 text-4xl leading-tight font-bold sm:text-5xl md:text-6xl md:leading-[75px]">
                   Find the Right Healthcare Whenever You Need.
                 </h1>
-                <p className="scroll-animate mt-4 mb-4 text-xl leading-8 font-normal sm:text-2xl md:leading-10">
+                <p className="scroll-animate mt-4 mb-4 text-xl leading-10 font-normal sm:text-2xl md:leading-10">
                   CommuniCare will help connect you with free healthcare
                   services nearby because you deserve to feel better.
                 </p>
@@ -70,7 +70,7 @@ function Home() {
               <h2 className="scroll-animate mb-5 text-center text-4xl font-bold text-neutral-800 md:text-left md:text-5xl">
                 Our App
               </h2>
-              <h5 className="font-weight-700 scroll-animate justify-center text-center text-xl leading-8 text-neutral-800 md:text-left md:text-2xl md:leading-10">
+              <h5 className="font-weight-700 scroll-animate justify-center text-center text-xl leading-10 text-neutral-800 md:text-left md:text-2xl md:leading-10">
                 Discover affordable services nearby with our easy-to-use app.
               </h5>
               <div className="flex justify-center md:justify-start">
@@ -106,7 +106,7 @@ function Home() {
               <h2 className="scroll-animate mb-5 text-4xl font-bold text-neutral-800 md:text-5xl">
                 About Us
               </h2>
-              <p className="font-weight-700 scroll-animate justify-center text-xl leading-8 text-neutral-800 md:text-2xl md:leading-10">
+              <p className="font-weight-700 scroll-animate justify-center text-xl leading-10 text-neutral-800 md:text-2xl md:leading-10">
                 We are committed to building healthier communities by spreading
                 awareness of affordable resources.
               </p>
@@ -127,11 +127,11 @@ function Home() {
               <h2 className="scroll-animate mb-5 text-4xl font-bold text-neutral-900 md:text-5xl">
                 Workshops
               </h2>
-              <p className="font-weight-700 scroll-animate justify-center pb-4 text-xl leading-8 text-neutral-900 md:text-2xl md:leading-10">
+              <p className="font-weight-700 scroll-animate justify-center pb-4 text-xl leading-10 text-neutral-900 md:text-2xl md:leading-10">
                 Join one of our workshops for personalized support and guidance
                 on your options.
               </p>
-              <h5 className="scroll-animate justify-center text-xl leading-8 font-bold text-neutral-900 md:text-2xl md:leading-10">
+              <h5 className="scroll-animate justify-center text-xl leading-10 font-bold text-neutral-900 md:text-2xl md:leading-10">
                 Stay Tuned!
               </h5>
             </div>
