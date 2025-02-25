22342514b297da206b5b9128b0ac053eb627133c
TailaiYing <taiying9627@gmail.com>
2025-02-25 11:52:02 -0500

    browser router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home.tsx";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About.tsx";
 import Team from "./pages/Team.tsx";
 import Contact from "./pages/Contact.tsx";
